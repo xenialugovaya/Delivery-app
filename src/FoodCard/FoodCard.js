236bb9f750b6dd48144c3f25cc7db8c019d86216
Ksenia Lugovaya <xenia.trofimova@gmail.com>
2020-06-07 23:02:07 +0300

    Added food card

---
+++ src/FoodCard/FoodCard.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+}));
+
+export default function FoodCard(props) {
+  const {title, price, shortDescription, description, image} = props;
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        title={title}
+      />
+      <CardMedia
+        className={classes.media}
+        image={image}
+        title={title}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+           {shortDescription}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+          <Typography>
+            {price}
+          </Typography>
+        <IconButton aria-label="add to cart">
+          <AddCircleIcon fontSize="large" color="primary" />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>
+             {description}
+          </Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+}
